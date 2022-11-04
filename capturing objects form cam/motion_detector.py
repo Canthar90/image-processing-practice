a93a2703700bc3bb61750c395df0aaef53593431
Canthar90 <d.cieslak90@gmail.com>
2022-11-04 11:32:38 +0100

    Added csv data storage

--- capturing objects form cam/motion_detector.py
+++ capturing objects form cam/motion_detector.py
@@ -1,18 +1,20 @@
-import cv2, time
+import cv2, time, pandas
+from datetime import datetime
 
 
 first_frame = None
-
+status_list = [None, None]
 video = cv2.VideoCapture(0)
+rec_times = []
 time.sleep(5) #delay for me to hide from camera :]
 flag = False
 delay_cycles_nr = 0
+df = pandas.DataFrame(columns=["Start", "End"])
 
 while True:
     
     check, frame = video.read()
-
-
+    status = 0
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0) #blurring removing the noise from image
     
@@ -30,29 +32,47 @@ while True:
     
     delta_frame = cv2.absdiff(first_frame, gray)  #comparing frames
     
-    thresh_delta = cv2.threshold(delta_frame, 48, 255, cv2.THRESH_BINARY)[1]
+    thresh_delta = cv2.threshold(delta_frame, 50, 255, cv2.THRESH_BINARY)[1]
     thresh_delta = cv2.dilate(thresh_delta, None, iterations=3)
     
     (cnts, _) =cv2.findContours(thresh_delta.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #creating contures
     
     for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 1500:
             continue
+        status = 1
         
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
     
+    status_list.append(status)
+    if status_list[-1] == 1 and status_list[-2] ==0: # rising edge detection
+        rec_times.append(datetime.now())
+    if status_list[-1] == 0 and status_list[-1] == 1: # falling edge detection
+        rec_times.append(datetime.now())
+        
     cv2.imshow("Delta frame" ,delta_frame)
     cv2.imshow("first frame", first_frame)
     cv2.imshow("treshold frame", thresh_delta)
     
     cv2.imshow("Capturing", frame)
-
-    key = cv2.waitKey(20)
+    
+    
+    key = cv2.waitKey(1)
     if key==ord('q'):
+        if status == 1:
+            rec_times.append(datetime.now())
         break
     
-video.release()
+print(status_list)
+print(rec_times)
 
+for i in range(0, len(rec_times)-1, 2):
+    print(i)
+    print(i+1)
+    print(len(rec_times))
+    df = df.append({"Start": rec_times[i], "End": rec_times[i+1]}, ignore_index=True)    
+df.to_csv("Times.csv")
+video.release()
 
 cv2.destroyAllWindows()
